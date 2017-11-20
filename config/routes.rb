2357637a1821015baa14d19fb078036e28015c29
Elisebassett <elise@iileaders.com>
2017-11-20 15:24:11 -0700

    boilerplate

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+  root to: "home#index"
+
+  resources :users, only: [:index, :show]
+  
+  
+end

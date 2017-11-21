46f5826dc5105e9c79830ef607e91edd500633b0
Elisebassett <elise@iileaders.com>
2017-11-20 21:58:50 -0700

    fixes #18 fixes #23 fixes #25 fixes #15

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,40 @@
+$(function () {
+	$('.job_make_changes').hide();
+	
+	$('.job_edit').on('click', function(){
+		var job_edit_form = $(this).closest('.job_edit_form');
+		
+		job_edit_form.find('.job_edit_field').attr('readonly', false)
+		job_edit_form.find('.job_first_field').focus();
+		$('.job_make_changes').show();
+	});
+
+	$('.job_make_changes').on('click', function(){
+		$(this).closest('.job_edit_form').find('.job_edit_field').attr('readonly', true);
+		$(this).hide();
+	});
+
+	$(".delete_job").on('ajax:success', function(){
+		$(this).closest('.job_info').remove();
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
